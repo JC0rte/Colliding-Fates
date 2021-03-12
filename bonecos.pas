727531d9f74b3db341d4f45268ae5fa95467f8aa
atosmico <80548077+atosmico@users.noreply.github.com>
2021-03-12 20:44:46 +0000

    Add files via upload

---
+++ bonecos.pas
@@ -0,0 +1,34 @@
+Program Pzim ;
+Begin
+  
+	textcolor(white);
+  writeln;  
+  writeln('  ',#179,'',#136,'_');
+  writeln('  ',#197,'O',#206);
+  writeln('  / \',' ');
+  writeln;  
+  writeln(' ',#179,' ',#136,' _');
+  writeln(' ',#197,'/O\',#206);
+  writeln('  / \',#202,' ');
+	writeln; 
+  writeln(' ',#179,' ',#136,'_');
+  writeln(' ',#197,'/O',#206);
+  writeln('  ( ',#202,'');
+	writeln;                                                                   
+  writeln('  ',#179,'',#136,' _');
+  writeln('  ',#197,'O\',#206);
+  writeln('  / )',#202,' ');
+	
+ 
+  writeln; 
+  writeln('  ',#143,' ',#245);  
+  writeln(' (',#1214'\',#197);
+  writeln(' / )',#179);
+  
+  
+	writeln;                                                                   
+  writeln('   ',#131,' ');
+  writeln(' ',#170,'/O) ');
+  writeln('  ( \ ');
+  
+End.
