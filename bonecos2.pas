5d7978216e78c28c5851f0f9c78b1aef142393e3
atosmico <80548077+atosmico@users.noreply.github.com>
2021-03-12 23:12:33 +0000

    Add files via upload

---
+++ bonecos2.pas
@@ -0,0 +1,64 @@
+Program Pzim ;
+Begin
+  
+  //KNIGHT
+  
+	textcolor(white);
+  writeln;  
+  writeln('  ',#179,'',#136,'_');
+  writeln('  ',#197,'Y',#206);                  //Costas
+  writeln('  / \',' ');
+  writeln;  
+  writeln(' ',#179,' ',#136,' _');
+  writeln(' ',#197,'/'#1214'\',#206);           //Frente
+  writeln('  / \',#202,' ');
+	writeln; 
+  writeln(' ',#179,' ',#136,'_');
+  writeln(' ',#197,'/'#1214,#206);              //Esquerda
+  writeln('  ( ',#202,'');
+	writeln;                                                                   
+  writeln('  ',#179,#136,' _');
+  writeln('  ',#197,#1214'\',#206);             //Direita
+  writeln('  / )',#202,' ');
+	
+  //MAGE
+ 
+  writeln; 
+  writeln('  ',#143,#245);  
+  writeln(' (H',#197);                          //Costas
+  writeln(' /_\');
+  writeln; 
+  writeln('  ',#143,' ',#245);  
+  writeln(' /M\',#197);                         //Frente
+  writeln(' /_\',#179);
+  writeln; 
+  writeln(' ',#245,' ',#143);  
+  writeln(' ',#197'/H)');                       //Esquerda
+  writeln(' ',#179,'(_\');
+  writeln; 
+  writeln('  ',#143,' ',#245);  
+  writeln(' (M\',#197);                         //Direita
+  writeln(' /_)',#179);
+  
+  //GUNSLINGER
+  
+	writeln;                                                                   
+  writeln('   ',#131,' ');
+  writeln(' ',#170,'/O) ');
+  writeln('  ( \ ');
+  writeln;                                                                   
+  writeln('   ',#131,' ');
+  writeln(' ',#170,'/O)',#197#196#196);
+  writeln('  ( \ ');
+  writeln;                                                                   
+  writeln('   ',#131,' ');
+  writeln(' ',#170,'/O) ');
+  writeln('  ( \ ');
+  writeln;                                                                   
+  writeln('   ',#131,' ');
+  writeln(' ',#170,'/O) ');
+  writeln('  ( \ ');
+  
+  
+ 
+End.
